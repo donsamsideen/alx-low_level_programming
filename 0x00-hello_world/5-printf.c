4b78781c5f9900b75f52ff3da9b65606bac7fafb
donsamsideen <kunleyguy@gmail.com>
2022-09-08 15:42:10 -0700

    ll

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,45 +1,22 @@
-nclude <stdio.h>
-
-
-
-
-
+#include <stdio.h>
 
 
 /**
-
-  *
-
-   *  * main - Entry point
-
-    *
-
-     *   *
-
-      *
-
-       *    * Return: Always 0 (Success)
-
-        *
-
-         *     */
-
-
-
-
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *    * Return: Always 0 (Success) 
+ *
+ *    */
 
 
 
 int main(void)
 
-
-
 {
 
+		print("With propert grammar, but the outcome is a piece of art,\n");
 
-
-			printf("with proper grammar, but the outcome is a piece of art,\n");
-
-
-
-						return (0);
+			return (0);
+}
