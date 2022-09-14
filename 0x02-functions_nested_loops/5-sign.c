4ca46550b7ed9cc96d9b10e61bd7cae6d0f8fc42
donsamsideen <kunleyguy@gmail.com>
2022-09-13 19:57:04 -0700

    coding

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,19 +1,21 @@
 #include "main.h"
 /**
- * _abs - absolute value
- * @n: integer
- *
- * Return: return absolute valuer of a number
- */
-int _abs(int n)
+* print_sign - function Checks for -ve, +ve or 0 integers
+* @n: This is a number assigned
+* Return: 0 if zero, 1 if > 0 and -1 if < 0
+*/
+int print_sign(int n)
 {
-	if (n < 0)
-	{
-		n = n * -1;
-		return (n);
-	}
-	else
-	{
-		return (n);
-	}
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+else if (n < 0)
+{
+_putchar('-');
+return (-1);
+}
+_putchar('0');
+return (0);
 }
