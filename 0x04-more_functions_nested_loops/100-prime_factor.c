8d1c5c459ce27f566678fa8ca6ae788b7c9aabe7
donsamsideen <kunleyguy@gmail.com>
2022-09-15 18:42:58 -0700

    coding

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,31 +1,16 @@
-#include "main.h"
+#include <stdio.h>
 /**
-* print_number - this function prints an integer
-* @n: input integer, a number
-* Return: void
+* main - this function prints prime factors
+* Return: 0
 */
-void print_number(int n)
+int main(void)
 {
-unsigned int i, j, count;
-if (n < 0)
+unsigned long int i = 3, n = 612852475143;
+for (; i < 12057; i += 2)
 {
-_putchar(45);
-i = n * -1;
+while (n % i == 0 && n != i)
+n /= i;
 }
-else
-{
-i = n;
-}
-j = i;
-count = 1;
-while (j > 9)
-
-{
-j /= 10;
-count *= 10;
-}
-for (; count >= 1; count /= 10)
-{
-_putchar(((i / count) % 10) + 48);
-}
+printf("%lu\n", n);
+return (0);
 }
