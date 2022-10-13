b881f753a53639ca11729130a7fec2cbb40c3fe0
donsamsideen <kunleyguy@gmail.com>
2022-10-13 11:18:25 +0100

    preprocessor

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,17 +1,6 @@
-#include "0-object_like_macro.h"
-#include "0-object_like_macro.h"
-#include <stdio.h>
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
 
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    int s;
+#define SIZE 1024
 
-    s = 98 + SIZE;
-    printf("%d\n", s);
-    return (0);
-}
+#endif
